94070d0b3c9cbf8b77020b40a0510089deee6ba4
ansab1 <ansab@rethinkux.com>
2021-10-09 22:39:41 +0530

    Added initial UI elements.

---
+++ app/src/main/java/com/devansab/major1/views/activities/HomeActivity.kt
@@ -0,0 +1,22 @@
+package com.devansab.major1.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.devansab.major1.R
+import com.google.android.material.tabs.TabLayout
+
+class HomeActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        initView()
+    }
+
+    fun initView(){
+
+    }
+}
--- app/src/main/java/com/devansab/major1/views/activities/MainActivity.kt
+++ app/src/main/java/com/devansab/major1/views/activities/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.devansab.major1
+package com.devansab.major1.views.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devansab.major1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var intent = Intent(this, HomeActivity::class.java);
+        startActivity(intent);
     }
 }
