1b00c0b00da5741fb9656b9f05fbc0912c60af3d
Mohd Ansab Parvez <ansab0parvez@gmail.com>
2021-11-20 23:32:38 +0530

    Added saving auth token in shared pref. Added single request queue for volley.

--- app/src/main/java/com/devansab/major1/utils/MainApplication.kt
+++ app/src/main/java/com/devansab/major1/utils/MainApplication.kt
@@ -1,20 +1,40 @@
 package com.devansab.major1.utils
 
 import android.app.Application
+import android.content.Context
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.firebase.auth.FirebaseAuth
 
 class MainApplication : Application() {
 
+    private val requestQueue = Volley.newRequestQueue(applicationContext);
+
     override fun onCreate() {
         super.onCreate()
+
+        addAuthTokenChangeListener()
     }
 
-    private fun addAuthTokenChangeListener(){
+    companion object{
+        private val instance = MainApplication()
+        fun getInstance() : MainApplication{
+            return instance
+        }
+    }
+
+    public fun addToRequestQueue(request: JsonObjectRequest) {
+        requestQueue.add(request)
+    }
+
+    private fun addAuthTokenChangeListener() {
         val user = FirebaseAuth.getInstance().currentUser ?: return;
 
         FirebaseAuth.getInstance().addIdTokenListener(FirebaseAuth.IdTokenListener { it ->
             it.getAccessToken(true).addOnSuccessListener { res ->
-                val token = res.token;
+                val token = res.token
+                SharedPrefManager.getInstance(this)?.setAuthToken(token);
             }
         })
     }
