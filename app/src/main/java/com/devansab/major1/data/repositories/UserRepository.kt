a40dc2dd267841a38676aaa59ef10c2fea2b4dd3
Mohd Ansab Parvez <ansab0parvez@gmail.com>
2021-11-20 23:33:07 +0530

    Added check, if the user if registered or not.

--- app/src/main/java/com/devansab/major1/data/repositories/UserRepository.kt
+++ app/src/main/java/com/devansab/major1/data/repositories/UserRepository.kt
@@ -1,27 +1,34 @@
 package com.devansab.major1.data.repositories
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.devansab.major1.utils.DebugLog
+import com.devansab.major1.utils.MainApplication
 import com.google.firebase.auth.FirebaseAuth
 import java.util.HashMap
 
 class UserRepository(application: Application) {
 
+    private val isUserRegisteredLiveData = MutableLiveData<Boolean>();
+
     public fun isUserRegistered() {
         val user = FirebaseAuth.getInstance().currentUser ?: return;
         user.getIdToken(false).addOnSuccessListener {
             DebugLog.i("ansab", "token: " + it.token);
+            checkRegisteredUser(it.token.toString())
         }
     }
+
 
     private fun checkRegisteredUser(token: String) {
         val url =
             "https://us-central1-major1-99a4c.cloudfunctions.net/userV1/isRegistrationFinished"
-        val networkRequest: JsonObjectRequest =
+        val jsonObjectRequest: JsonObjectRequest =
             object : JsonObjectRequest(Request.Method.GET, url, null,
                 Response.Listener { response ->
 
@@ -36,6 +43,11 @@ class UserRepository(application: Application) {
                     return params
                 }
             };
+        MainApplication.getInstance().addToRequestQueue(jsonObjectRequest);
+    }
+
+    public fun getRegisterUserLiveData() : LiveData<Boolean> {
+        return isUserRegisteredLiveData
     }
 
 }
--- app/src/main/java/com/devansab/major1/views/activities/MainActivity.kt
+++ app/src/main/java/com/devansab/major1/views/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devansab.major1.views.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.devansab.major1.MainViewModel
 import com.devansab.major1.R
@@ -14,17 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val user = FirebaseAuth.getInstance().currentUser;
-        if (user == null) {
-            var intent = Intent(this, LoginActivity::class.java);
-            startActivity(intent);
-            finish();
-            return;
-        }
-
         val viewModel = ViewModelProvider(
             this, ViewModelProvider.AndroidViewModelFactory
                 .getInstance(application)
         ).get(MainViewModel::class.java);
+
+        val user = FirebaseAuth.getInstance().currentUser;
+        if (user == null) {
+            val intent = Intent(this, LoginActivity::class.java);
+            startActivity(intent);
+            finish();
+            return;
+        }
+        
+        viewModel.isUserRegistered()
+
+        viewModel.getRegisterUserLiveData().observe(this, Observer { isRegistered ->
+            if (isRegistered) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
+
     }
 }
