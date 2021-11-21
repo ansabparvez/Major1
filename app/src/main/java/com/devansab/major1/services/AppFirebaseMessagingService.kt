8f5530044e7fe85ffa5d5ef316aca210949838ad
Mohd Ansab Parvez <ansab0parvez@gmail.com>
2021-11-22 00:16:22 +0530

    Added Firebase Messaging Service to get notifications.

--- app/src/main/java/com/devansab/major1/services/AppFirebaseMessagingService.kt
+++ app/src/main/java/com/devansab/major1/services/AppFirebaseMessagingService.kt
@@ -1,6 +1,7 @@
 package com.devansab.major1.services
 
 import com.devansab.major1.utils.DebugLog
+import com.devansab.major1.utils.SharedPrefManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -9,6 +10,7 @@ class AppFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         DebugLog.i(this, "fcm token: "+token)
         super.onNewToken(token)
+        SharedPrefManager.getInstance(baseContext).setFCMToken(token)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
