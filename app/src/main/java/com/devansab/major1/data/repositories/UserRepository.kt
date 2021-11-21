297521ed81443fc85d31a27e361c5d86d03140aa
Mohd Ansab Parvez <ansab0parvez@gmail.com>
2021-11-22 00:17:31 +0530

    Added user registration screen.

--- app/src/main/java/com/devansab/major1/data/repositories/UserRepository.kt
+++ app/src/main/java/com/devansab/major1/data/repositories/UserRepository.kt
@@ -7,21 +7,26 @@ import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
 import com.devansab.major1.utils.DebugLog
 import com.devansab.major1.utils.MainApplication
+import com.devansab.major1.utils.SharedPrefManager
 import com.google.firebase.auth.FirebaseAuth
+import org.json.JSONObject
 import java.util.HashMap
 
 class UserRepository(val application: Application) {
 
     private val isUserRegisteredLiveData = MutableLiveData<Boolean>();
+    private val isUserNameAvailableLiveData = MutableLiveData<Boolean>();
+    private val userRegistrationLiveData = MutableLiveData<Boolean>();
 
     public fun isUserRegistered() {
         val user = FirebaseAuth.getInstance().currentUser ?: return;
+        DebugLog.i(this, "getting token")
         user.getIdToken(false).addOnSuccessListener {
             DebugLog.i("ansab", "token: " + it.token);
             checkRegisteredUser(it.token.toString())
+            SharedPrefManager.getInstance(application).setAuthToken(it.token.toString());
         }
     }
 
@@ -32,7 +37,6 @@ class UserRepository(val application: Application) {
         val jsonObjectRequest: JsonObjectRequest =
             object : JsonObjectRequest(Request.Method.GET, url, null,
                 Response.Listener { response ->
-                    DebugLog.i("ansab", response.toString())
                     if (response.getBoolean("success")) {
                         isUserRegisteredLiveData.value =
                             response.getBoolean("registrationFinished")
@@ -50,11 +54,78 @@ class UserRepository(val application: Application) {
                     return params
                 }
             };
-        MainApplication.instance.getRequestQueue().add(jsonObjectRequest);
+        MainApplication.instance.addToRequestQueue(jsonObjectRequest)
     }
 
     public fun getRegisterUserLiveData(): LiveData<Boolean> {
         return isUserRegisteredLiveData
+    }
+
+    public fun getIsUserNameAvailableLiveData(): LiveData<Boolean> {
+        return isUserNameAvailableLiveData
+    }
+
+    public fun getUserRegistrationLiveData(): LiveData<Boolean> {
+        return userRegistrationLiveData
+    }
+
+    public fun checkUserNameAvailable(userName: String) {
+        val url =
+            "https://us-central1-major1-99a4c.cloudfunctions.net/userV1/isUserNameAvailable?userName=$userName"
+        val jsonObjectRequest: JsonObjectRequest =
+            object : JsonObjectRequest(Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    DebugLog.i("ansab", "response: ($response.toString())")
+                    if (response.getBoolean("success")) {
+                        isUserNameAvailableLiveData.value =
+                            response.getBoolean("isUserNameAvailable")
+                    } else {
+                        isUserNameAvailableLiveData.value = false
+                    }
+                },
+                Response.ErrorListener { }
+            ) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val params: MutableMap<String, String> = HashMap()
+                    val token = SharedPrefManager.getInstance(application).getAuthToken();
+                    params["Authorization"] = "Bearer $token"
+                    params["Content-Type"] = "application/json"
+                    return params
+                }
+            };
+        MainApplication.instance.addToRequestQueue(jsonObjectRequest)
+    }
+
+    public fun registerUser(userData: HashMap<String, String>) {
+        val url =
+            "https://us-central1-major1-99a4c.cloudfunctions.net/userV1/completeRegistration"
+        val jsonObjectRequest: JsonObjectRequest =
+            object : JsonObjectRequest(Request.Method.POST, url, null,
+                Response.Listener { response ->
+                    DebugLog.i("ansab", response.toString())
+                    userRegistrationLiveData.value = response.getBoolean("success")
+                },
+                Response.ErrorListener { }
+            ) {
+                override fun getBodyContentType(): String {
+                    return "application/json"
+                }
+
+                override fun getBody(): ByteArray {
+                    return JSONObject(userData as Map<String, String>).toString().toByteArray()
+                }
+
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val params: MutableMap<String, String> = HashMap()
+                    val token = SharedPrefManager.getInstance(application).getAuthToken();
+                    params["Authorization"] = "Bearer $token"
+                    params["Content-Type"] = "application/json"
+                    return params
+                }
+            };
+        MainApplication.instance.addToRequestQueue(jsonObjectRequest);
     }
 
 }
---
+++ app/src/main/java/com/devansab/major1/viewmodles/UserRegistrationViewModel.kt
@@ -0,0 +1,56 @@
+package com.devansab.major1.viewmodles
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.devansab.major1.data.repositories.UserRepository
+import com.devansab.major1.utils.SharedPrefManager
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class UserRegistrationViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var userRepository = UserRepository(application)
+
+    public fun isUserNameAvailable(userName: String) {
+        userRepository.checkUserNameAvailable(userName)
+    }
+
+    public fun getUserNameAvailableLiveData(): LiveData<Boolean> {
+        return userRepository.getIsUserNameAvailableLiveData()
+    }
+
+    public fun completeRegistration(name: String, userName: String) {
+        val userDataMap = HashMap<String, String>()
+        userDataMap["name"] = name
+        userDataMap["userName"] = userName
+        setFCMTokenAndRegister(userDataMap)
+
+    }
+
+    private fun setFCMTokenAndRegister(userDataMap: HashMap<String, String>) {
+        val token = SharedPrefManager.getInstance(getApplication()).getFCMToken();
+        if (token != null) {
+            userDataMap["fcmToken"] = token
+            userRepository.registerUser(userDataMap)
+            return
+        }
+
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val newToken = task.result;
+                    userDataMap["fcmToken"] = newToken
+                    userRepository.registerUser(userDataMap)
+                }
+            })
+    }
+
+    public fun getUserRegistrationLiveData(): LiveData<Boolean>{
+        return userRepository.getUserRegistrationLiveData()
+    }
+
+}
