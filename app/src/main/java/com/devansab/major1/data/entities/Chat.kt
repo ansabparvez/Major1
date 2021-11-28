0d3763dd63f62e375eecaf7653d35a9625824227
Mohd Ansab Parvez <ansab0parvez@gmail.com>
2021-11-28 23:19:55 +0530

    Deleted unused model.

--- app/src/main/java/com/devansab/major1/data/entities/Chat.kt
+++
@@ -1,8 +0,0 @@
-package com.devansab.major1.data.entities
-
-data class Chat(val message: String, val time: Long, val type: Int) {
-    companion object {
-        val TYPE_SENT: Int = 1;
-        val TYPE_RECEIVED: Int = 2;
-    }
-}
--- app/src/main/java/com/devansab/major1/data/entities/ChatPreview.kt
+++
@@ -1,4 +0,0 @@
-package com.devansab.major1.data.entities
-
-
-data class ChatPreview(val userName: String, val lastMessage : String, val date : String)
