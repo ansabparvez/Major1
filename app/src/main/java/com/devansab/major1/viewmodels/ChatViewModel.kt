be5338c638c755275107548b839fc5b621fe947e
Mohd Ansab Parvez <ansab0parvez@gmail.com>
2021-12-02 00:24:20 +0530

    Set up chat activity and view model.

--- app/src/main/java/com/devansab/major1/viewmodels/ChatViewModel.kt
+++ app/src/main/java/com/devansab/major1/viewmodels/ChatViewModel.kt
@@ -1,4 +1,14 @@
 package com.devansab.major1.viewmodels
 
-class ChatViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.devansab.major1.data.repositories.MessageRepository
+import com.devansab.major1.data.repositories.UserRepository
+
+class ChatViewModel(application: Application) : AndroidViewModel(application) {
+    private val messageRepository = MessageRepository(application)
+    private val userRepository = UserRepository(application)
+
+    fun getUserByUsername(username: String) = userRepository.getUserByUsername(username)
+
 }
--- app/src/main/java/com/devansab/major1/views/activities/ChatActivity.kt
+++ app/src/main/java/com/devansab/major1/views/activities/ChatActivity.kt
@@ -2,13 +2,19 @@ package com.devansab.major1.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.RecyclerView
 import com.devansab.major1.R
+import com.devansab.major1.viewmodels.ChatViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class ChatActivity : AppCompatActivity() {
 
     private lateinit var rvChat: RecyclerView
-    //private lateinit var view
+    private lateinit var viewmodel: ChatViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,20 @@ class ChatActivity : AppCompatActivity() {
 
     private fun initViews() {
         rvChat = findViewById(R.id.rv_chat_chats)
+        viewmodel = ViewModelProvider(
+            this, ViewModelProvider.AndroidViewModelFactory
+                .getInstance(application)
+        )[ChatViewModel::class.java]
+
+        val userName = intent.getStringExtra("userName");
+
+        viewmodel.viewModelScope.launch {
+            viewmodel.getUserByUsername(userName!!).collect {
+                findViewById<TextView>(R.id.tv_chat_userName)
+                    .text = it.name
+            }
+        }
+
 
     }
 }
