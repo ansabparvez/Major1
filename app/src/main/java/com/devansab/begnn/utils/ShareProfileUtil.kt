cf9ea2c33604f9000d044b91af7255cd096521f5
Mohd Ansab Parvez <ansab0parvez@gmail.com>
2022-05-11 22:36:11 +0530

    Added share user profile as deep link.

--- app/src/main/java/com/devansab/begnn/utils/ShareProfileUtil.kt
+++ app/src/main/java/com/devansab/begnn/utils/ShareProfileUtil.kt
@@ -11,7 +11,7 @@ class ShareProfileUtil {
             val shareIntent = Intent()
             shareIntent.action = Intent.ACTION_SEND
             shareIntent.type = "text/plain"
-            shareIntent.putExtra(Intent.EXTRA_TEXT, "I am now on Begnn app. Search for $userName")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "I am now on Begnn app. Click on this link and start chatting with me https://devansab.com/begnn/?user=$userName")
             context.startActivity(Intent.createChooser(shareIntent, "Share to"))
         }
     }
